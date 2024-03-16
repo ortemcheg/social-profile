249b624804fd5c20bcd678f65959f5e216ac84f3
Tyoma Deev <tyoma.deev@gmail.com>
2024-03-16 11:11:36 +0100

    Extracts styles for App component

---
+++ src/App.styles.ts
@@ -0,0 +1,24 @@
+import styled from "@emotion/styled";
+export const BackgroundWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #000;
+  min-height: 100%;
+  font-family: sans-serif;
+`;
+export const CardWrapper = styled.div`
+  width: 327px;
+  margin: 0 auto;
+  background: #1f1f1f;
+  border-radius: 12px;
+  padding: 24px;
+`;
+
+export const ImagePlaceHolder = styled.div`
+  width: 88px;
+  height: 88px;
+  margin: 0 auto;
+  background-color: #666;
+  border-radius: 50%;
+`;
